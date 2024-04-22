cf966a8588f3f03c494602fb781d2611cf15f562
HRN_Harshit <hrnharshit@gmail.com>
2024-04-22 12:26:31 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import tensorflow as tf
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+
+class_names = ['MildDementia', 'ModerateDementia', 'NonDementia', 'VeryMildDementia']
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model=tf.keras.models.load_model('./alzheimer_model.keras')
+    return model
+
+model = load_model()
+st.write("""
+    # Alzheimer's Disease Detection
+    """)
+
+file = st.file_uploader("Please upload an image file", type=["jpg", "png"])
+
+def import_and_predict(image_data, model):
+    size = (180, 180)
+    # image = ImageOps.fit(image_data, size)
+    image = np.asarray(image_data)
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)    
+    img_reshape = img[np.newaxis,...]
+
+    prediction = model.predict(img_reshape)
+    return prediction
+
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    predictions = import_and_predict(image, model)
+    score = tf.nn.softmax(predictions[0])
+    st.write("Predictions:", dict(zip(class_names, predictions[0])))
+    st.write("This image most likely belongs to {} with a {:.2f} percent confidence".format(class_names[np.argmax(score)], 100 * np.max(score)))
+        
